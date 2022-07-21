50c0d33e012eacb8376a8e3c4bb321f989c4948a
hll <595188175@qq.com>
2022-07-21 21:59:01 +0800

    0-init

---
+++ src/test/java/com/demo/exportdemo/ExportDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.exportdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExportDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
