a800dc0434fa0c0064759477ed940cf51a6de245
hll <595188175@qq.com>
2022-07-22 13:27:58 +0800

    5.集成热部署 (1)加入依赖包 (2)setting - Compiler Build...automatically 打勾 (3)找到Registry指令 complier.automaker.allow.when.app.running 打勾

--- src/main/java/com/demo/exportdemo/controller/TestController.java
+++ src/main/java/com/demo/exportdemo/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.demo.exportdemo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,13 +10,16 @@ import java.util.Map;
 @RestController
 public class TestController {
 
+    @Value("${test.hello:TEST}")
+    private String name;
+
     @RequestMapping ("/hello")
     public  String hello(){
         return "恭喜你，已经成功的迈出第一步";
     }
 
     @RequestMapping ("/hello/post")
-    public  String helloPost(String name){
+    public  String helloPost(){
         return "恭喜你，又学到了一些新东西："+name;
     }
 
