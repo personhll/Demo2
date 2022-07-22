750833d4f76e04403dcef3a8906821f37b54ae4b
hll <595188175@qq.com>
2022-07-22 19:06:28 +0800

    0.9 集成Mybatis generator ，生成Product表代码

--- doc/all.sql
+++ doc/all.sql
@@ -7,3 +7,15 @@ create table test(
                      update_time datetime comment '更新日期',
                      primary key (id)
 )engine=innodb default charset=utf8mb4 comment '测试';
+
+
+drop table if exists product;
+create table product(
+                     id  bigint not null comment  'id' auto_increment,
+                     prod_code varchar(50) comment  '产品编码',
+                     prod_name varchar(50) comment  '产品名称',
+                     prod_price int comment '单价',
+                     create_time datetime comment '创建日期',
+                     update_time datetime comment '更新日期',
+                     primary key (id)
+)engine=innodb default charset=utf8mb4 comment '产品';
---
+++ src/main/java/com/demo/exportdemo/domain/Product.java
@@ -0,0 +1,81 @@
+package com.demo.exportdemo.domain;
+
+import java.util.Date;
+
+public class Product {
+    private Long id;
+
+    private String prodCode;
+
+    private String prodName;
+
+    private Integer prodPrice;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getProdCode() {
+        return prodCode;
+    }
+
+    public void setProdCode(String prodCode) {
+        this.prodCode = prodCode;
+    }
+
+    public String getProdName() {
+        return prodName;
+    }
+
+    public void setProdName(String prodName) {
+        this.prodName = prodName;
+    }
+
+    public Integer getProdPrice() {
+        return prodPrice;
+    }
+
+    public void setProdPrice(Integer prodPrice) {
+        this.prodPrice = prodPrice;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", prodCode=").append(prodCode);
+        sb.append(", prodName=").append(prodName);
+        sb.append(", prodPrice=").append(prodPrice);
+        sb.append(", createTime=").append(createTime);
+        sb.append(", updateTime=").append(updateTime);
+        sb.append("]");
+        return sb.toString();
+    }
+}
