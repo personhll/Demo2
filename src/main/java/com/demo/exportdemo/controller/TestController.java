6d37e17bdd2e2fd9bc3cac2326ee0935f243c44d
hll <595188175@qq.com>
2022-07-22 12:54:16 +0800

    4.增加测试类

--- src/main/java/com/demo/exportdemo/controller/TestController.java
+++ src/main/java/com/demo/exportdemo/controller/TestController.java
@@ -3,12 +3,19 @@ package com.demo.exportdemo.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 public class TestController {
 
     @RequestMapping ("/hello")
     public  String hello(){
         return "恭喜你，已经成功的迈出第一步";
+    }
+
+    @RequestMapping ("/hello/post")
+    public  String helloPost(String name){
+        return "恭喜你，又学到了一些新东西："+name;
     }
 
 }
